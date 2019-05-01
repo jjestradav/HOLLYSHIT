2de94eee01baad8152c23b6d7a091e789779e791
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-04-30 20:06:01 -0600

    generador finished

--- main.c
+++ main.c
@@ -1,49 +1,59 @@
-#include "poisson.c"
 #include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char** argv){
-int np;
-int pid;
-int memi;
-int mema;
-int durmi;
-int durma;
-int hlli;
-int lambda;
-if (argc< 8){
-    printf("DEBE de ingresar 8 argumentos");
-    printf("\n");
-    return 1;
-}else{
-np=atoi(argv[1]);    
-pid=atoi(argv[2]);
-memi=atoi(argv[3]);
-mema=atoi(argv[4]);
-durmi=atoi(argv[5]);
-durma=atoi(argv[6]);
-hlli=atoi(argv[7]);
-lambda=atoi(argv[8]);
-for (int i=1; i<argc; i++){
-    printf("%s", argv[i]);
-    printf("\n");
+#include <unistd.h>
+#include "poisson.c"
+#include "random.c"
+int main(int argc, char **argv)
+{
+    int np;     //numero de proceso
+    int pid;    //id de proceso
+    int memi;   // memoria minima
+    int mema;   //memoria maxima
+    int durmi;  // duracion minima
+    int durma;  // duracion  maxima
+    int hlli;   // hora de llegada inicial
+    int lambda; //lambda
+    if (argc < 8)
+    {
+        printf("DEBE de ingresar 8 argumentos");
+        printf("\n");
+        return 0;
+    }
+    else
+    {
+        np = atoi(argv[1]);
+        pid = atoi(argv[2]);
+        memi = atoi(argv[3]);
+        mema = atoi(argv[4]);
+        durmi = atoi(argv[5]);
+        durma = atoi(argv[6]);
+        hlli = atoi(argv[7]);
+        lambda = atoi(argv[8]);
+        creaTabla(lambda);
+        for (int i = 0; i < np; i++)
+        {
+            printf("%d", pid);
+            printf("\t");
+            printf("%d", hlli);
+            printf("\t");
+            printf("%d", ramdom_Between(durmi, durma));
+            printf("\t");
+            printf("%d", ramdom_Between(memi, mema));
+            printf("\n");
+            pid = pid + 1;
+            long double random= random_R();
+            int proba = valorPoisson(random);
+            hlli = hlli + proba;
+            sleep(1);
+        }
+        return 1;
+    }
+    return 0;
 }
-printf("%d", np);
-printf("\n");
-printf("%d", pid);
-printf("\n");
-printf("%d", memi);
-printf("\n");
-printf("%d", mema);
-printf("\n");
-printf("%d", durmi);
-printf("\n");
-printf("%d", durma);
-printf("\n");
-printf("%d", hlli);
-printf("\n");
-printf("%d", lambda);
-printf("\n");
-return 1;
-}
-    
-}
+
+//  printf("\n");
+// creaTabla(2);
+// printf("%d", valorPoisson(random_R()));
+// printf("\n");
+// for(int i=0; i< 3*2; i++){
+//     printf("%Lf", tablaPoisson[i]);
+//     printf("\n");
--- poisson.c
+++ poisson.c
@@ -1,6 +1,28 @@
-#include <stdio.h> 
+#include <stdlib.h> 
 #include <math.h>
-int kfactorial(int n){
+long double * tablaPoisson;
+
+void creaTabla(int lambda){
+    tablaPoisson = calloc(sizeof(long double), 3*lambda);
+    tablaPoisson[0] = exp(-1*lambda);
+    int i =0; int ifactorial = 1;
+    while(i++ <3*lambda){
+        ifactorial *=i;
+        tablaPoisson[i] = tablaPoisson[i-1]+
+                        tablaPoisson[0]*
+                        pow(lambda,i)/ifactorial;
+    }
+    tablaPoisson[i-1] =1.0;
+}
+
+int valorPoisson(long double r){
+    int respuesta =0;
+    while(r>tablaPoisson[respuesta++]);
+    return respuesta-1;   
+}
+
+
+/*int kfactorial(int n){
     int fac=1;
     for(int i=n; i>0; i--){
     fac= fac*i;
@@ -13,5 +35,5 @@ double calculapoisson(int lambda, int k){
     int factk= kfactorial(k);
     double result=(power*x)/factk;
     return result;
-}
+}*/
 
