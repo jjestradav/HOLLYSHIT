13c20e75f64bc3e293fe213bcf58941569b40090
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-06 20:57:23 -0600

    other changes

--- calendarizacion/main.c
+++ calendarizacion/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <string.h>
 #include <stdbool.h>
 #include "read.c"
 #include "list.c"
@@ -11,36 +12,49 @@ int main()
 {
     char *arg[4];
 
-    // FILE *fp;
-    // char *filename = "/home/jose/Documents/HOLLYSHIT/file.txt";
-    // read_gen(fp, filename, &head);
-    FILE *f;
-    char* name="config.txt";
-    read_config(f,name,arg);
+    FILE *fp;
+    char *filename = "/home/jose/Documents/HOLLYSHIT/file.txt";
+    read_gen(fp, filename, &head);
+    for (p *l = head; l != NULL; l = l->next)
+    {
+        printf("%d, %d, %d, %d\n", l->id, l->horallegada, l->duracion, l->tama);
+    }
+    dequeue(&head);
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("*********************************************************************************************************\n");
+    for (p *l = head; l != NULL; l = l->next)
+    {
+        printf("%d, %d, %d, %d\n", l->id, l->horallegada, l->duracion, l->tama);
+    }
 
-    // for (p *l = head; l != NULL; l = l->next)
-    // {
-    //     printf("%d, %d, %d, %d\n", l->id, l->horallegada, l->duracion, l->tama);
-    // }
+    // FILE *f;
+    // char* name="config.txt";
+    // read_config(f,name,arg);
+
+    // 
        
-       char* nombre1="Secuencial";
-       printf("%s",arg[0]);
-        if (arg[0] == nombre1)
-        {
-            printf("entro\n");
-            if (arg[1] == "BF")
-            {
-            }
-            if (arg[1] == "FIFO")
-            {
-            }
-            if (arg[1] == "WF")
-            {
-            }
-        }
-        if(arg[0]=="Paginacion"){
-            paginacion(arg);
-        }
+    //    char* nombre1="Secuencial";
+    //    printf("%s",arg[0]);
+    //     if (arg[0] == nombre1)
+    //     {
+    //         printf("entro\n");
+    //         if (arg[1] == "BF")
+    //         {
+    //         }
+    //         if (arg[1] == "FIFO")
+    //         {
+    //         }
+    //         if (arg[1] == "WF")
+    //         {
+    //         }
+    //     }
+    //     if(arg[0]=="Paginacion"){
+    //         paginacion(arg);
+    //     }
     
 
     return 0;
