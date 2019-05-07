2c683027f3b81fa65bb184ac62aa1bba512a4ae9
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-05-06 18:24:52 -0600

    some changes

---
+++ calendarizacion/memoria.c
@@ -0,0 +1,94 @@
+#ifndef LIST_C
+#define LIST_C
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bloque bloque;
+void push(bloque** head, int id, int tama);
+void pop(bloque* head);
+void print_list(bloque ** head);
+typedef struct bloque
+{
+    int id;
+    int tama;
+    struct bloque *next;
+}bloque;
+
+
+int cant; 
+void push(bloque** head, int id,int tama)
+{
+    bloque* current=*head;
+ bloque *temp = (bloque*)malloc(sizeof(bloque));
+ temp->id=id;
+ temp->tama=tama;
+ temp->next=NULL;
+
+if (current == NULL)
+	{
+		*head = temp;
+        cant++;
+	}
+	else
+	{
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = temp;
+        cant++;
+	}
+}
+void pop(bloque* head)
+{
+    bloque *t, *u;
+    int i, d, h, tam;
+    if (head == NULL)
+    {
+        printf("Linked list is already empty.\n");
+        return;
+    }
+    if (head->next == NULL)
+    {
+        i = head->id;
+        d = head->duracion;
+        h = head->horallegada;
+        tam = head->tama;
+
+        free(head);
+        head = NULL;
+        cant--;
+        // printf("%d deleted from end successfully.\n", n);
+        return;
+    }
+    t = head;
+    while (t->next != NULL)
+    {
+        u = t;
+        t = t->next;
+    }
+    i = t->id;
+    d= t->duracion;
+    h= t->horallegada;
+    tam= t->tama;
+    u->next = NULL;
+    free(t);
+    cant--; 
+    // printf("%d deleted from end successfully.\n", n);
+}
+
+void print_list(bloque ** head) {
+    bloque * current = *head;
+
+    while (current != NULL) {
+        printf("%d, %d, %d, %d\n", current->id,current->horallegada,current->duracion,
+        current->tama);
+        current = current->next;
+    }
+}
+
+
+
+
+
+
+#endif
