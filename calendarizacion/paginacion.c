60b6f0f5aaaa7baafde28097dd9050cf93d71576
jjestradav <jjestradav@gmail.com>
2019-05-12 18:50:39 -0600

    Almost almost done

--- calendarizacion/paginacion.c
+++ calendarizacion/paginacion.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 typedef struct pagina
 {
     int id;
@@ -7,6 +8,19 @@ typedef struct pagina
     int sobrante;
 } page;
 
+bool isDone(int filas, int columnas, page *mat[filas][columnas]){
+    for(int i=0; i<filas; i++){
+        for(int j=0; j<columnas; j++){
+            if((*mat) != NULL){
+                    if(mat[i][j]->id > 0){
+                        return false;
+                    }
+            }
+            
+        }
+    }
+    return true;
+}
 int isEmptyP(page **mat, int filas, int columnas)
 {
     int flag = 0;
@@ -22,14 +36,15 @@ int isEmptyP(page **mat, int filas, int columnas)
         return (flag == (filas * columnas) ? 0 : 1);
     }
 }
-void printBeginM(FILE *tex, char *filename, int tamaOri)
+
+void printBeginM(FILE *tex, char *filename, int tamaOri, int tamPag)
 {
 
     // tex = fopen(filename, "w+");
     //char* msg="\\documentclass[10pt,a4paper]{article}\n\\usepackage[utf8]{inputenc}\n\\begin{document}\n\\begin{center}\n";
     fputs("\\documentclass[10pt,a4paper]{article}\n\\usepackage[utf8]{inputenc}\n\\begin{document}\n\\begin{center}\n", tex);
-    fprintf(tex, "\\section*{Configuracion}\n\\begin{description}\n\\item[Algoritmo:] %s \n", "Paginacion");
-    fprintf(tex, "\\item[Tamaño total:] %d \n\\end{description}\n\\end{center}\n", tamaOri);
+    fprintf(tex, "\\section*{Simulación de Manejo de Memoria}\n\\begin{description}\n\\item[Algoritmo:] %s \n", "Paginacion");
+    fprintf(tex, "\\item[Tamaño total:] %d \n \\item[Tamaño de página:] %d\n\\end{description}\n\\end{center}\n", tamaOri,tamPag);
     //fclose(tex);
 }
 void printEndM(FILE * tex,char* filename){
@@ -56,15 +71,21 @@ void paginacion(char *arg[], p **head, FILE *tex, char *filename2)
             matriz[i][j] = NULL;
         }
     }
+    bool flag1 = false;
+    bool flag2= false;
+    bool done= false;
     int instante = 1;
-    printBeginM(tex, filename2, tammemo);
-    while (isEmpty(head) != 0 &&isEmptyP(*matriz, cantfilas, cantfilas) != 0)
+    printBeginM(tex, filename2, tammemo,tampage);
+    while (isEmpty(head) != 0 && isEmptyP(*matriz, cantfilas, cantfilas) != 0)
     {
-
+     flag1=flag2=done=false;
+     
         for (int i = 0; i < cantfilas; i++)
         {
             for (int j = 0; j < cantfilas; j++)
             {
+              
+              if( isEmpty(head) != 0){ 
 
                 if ((*head) != NULL && (*head)->horallegada <= instante)
                 {
@@ -94,6 +115,28 @@ void paginacion(char *arg[], p **head, FILE *tex, char *filename2)
                         }
                     }
                 }
+
+              }
+              else{
+                  if(!isDone(cantfilas,cantfilas,matriz)){
+                      
+                        //instante++;
+                        flag1=true;
+                        break;
+                  }
+                  else{
+                        flag1=true;
+                        done=true;
+                  }
+              }
+            
+                   if(flag1){
+                   flag2 = true;
+                   break;
+               }
+            }
+            if(flag2){
+                break;
             }
         }
         char * msg="\\begin{center}\n\nInstante: ";
@@ -142,6 +185,10 @@ void paginacion(char *arg[], p **head, FILE *tex, char *filename2)
         char * msg5="\\end{tabular}\n\\end{center}\n\\pagebreak\n";
         fprintf(tex,"%s",msg5);
         // printf("%s", "pagebreak");
+
+        if(flag1 && flag2 && done)
+            break;
+
         instante++;
     }
     printEndM(tex, filename2);
